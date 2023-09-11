b675e396afc0b5b8b5973edc52127d35ba877daf
Jesse Stahl <lomick2090@gmail.com>
2023-09-11 10:43:55 -0400

    cargo practice

---
+++ hello_cargo/target/debug/deps/hello_cargo-9e2e1085575930c2.d
@@ -0,0 +1,5 @@
+/Users/minkman/repos/Rust_Practice/hello_cargo/target/debug/deps/hello_cargo-9e2e1085575930c2: src/main.rs
+
+/Users/minkman/repos/Rust_Practice/hello_cargo/target/debug/deps/hello_cargo-9e2e1085575930c2.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/minkman/repos/Rust_Practice/hello_cargo/target/debug/hello_cargo: /Users/minkman/repos/Rust_Practice/hello_cargo/src/main.rs
